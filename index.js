686b7684434c6c65db6e7bee55c72f7822cf5d8f
MarwaBaniFadil <marwa.bassam.2002@gmail.com>
2025-07-06 13:38:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+$("button").on("click", function(){
+    
+let username = document.getElementById("username").value;
+let password = document.getElementById("pass").value;
+
+if ((username || password) == ""){
+    $("#result").css("color","red").text("Please fill all fields !!")
+}
+else if (username == "admin" && password == "12345"){
+    $("#result").css("color","green").text("Login Successful")
+    window.location.href = "home.html";
+}
+
+else {
+    $("#result").css("color","red").text("Invalid Credentials")
+
+}
+
+$("input").val("")
+
+})
+
+   $("#show_chBox").on("click",function(){
+    if ($("#show_chBox").prop("checked")){
+     $("#pass").attr("type","text")
+    }
+    else {
+        $("#pass").attr("type","password") 
+    }
+  
+   }) 
+   
